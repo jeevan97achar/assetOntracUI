6fa930de5956fc8c8a20d5a7705eabadaad1f97b
jeevan97achar <jeevanacharr1997@gmail.com>
2020-11-30 22:46:19 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import SideNav from './Components/Navigation/SideNav'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Users from './Components/Pages/SystemAdmin/Users'
+import Profile from './Components/Pages/SystemAdmin/Profile'
+import SignIn from './Components/Pages/SignIn/SignIn'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/users" component={Users} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/dashboard" component={SideNav} />
+        </Switch>
+      </Router>
     </div>
   );
 }
